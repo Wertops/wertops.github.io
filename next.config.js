ed905fad8d6268331b96782ac27671366f7db688
Ella Carter <jackrusselruby@gmail.com>
2023-10-16 21:25:19 -0400

    turned off  image optimization and added basePath and assetPrefix to next.config.js trying to make experience images work

--- next.config.js
+++ next.config.js
@@ -3,8 +3,10 @@
  */
 const nextConfig = {
     output: 'export',
-    images: {unoptimized:true},
-    distDir: 'docs'
+    //images: {unoptimized:true},
+    distDir: 'docs',
+    basePath: '',
+    assetPrefix: '/'
     
     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
     // trailingSlash: true,
