004f33da62b3fe515e4c03269f98b6c3276741bb
Ella Carter <jackrusselruby@gmail.com>
2023-10-16 20:38:00 -0400

    edited next.config.js so that it will statically build and export the website as an html file to the /docs directory. Github pages allow for the /docs folder to automatically be used as the home directory, so getting next.js to simply build everything to docs made my life easier. I also deleted two files from earlier troubleshooting and iterations of this app that were no longer necessary.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-basePath: 'wertops.github.io/personal-website'
-
-module.exports = nextConfig
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: 'export',
+    images: {unoptimized:true},
+    distDir: 'docs'
+    
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // trailingSlash: true,
+   
+    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    // skipTrailingSlashRedirect: true,
+   
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
+  }
+  //images.unoptimized = true
+  module.exports = nextConfig
